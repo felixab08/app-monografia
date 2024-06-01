d0b5a902f9aa36ccd916677a4f096c57b87b60c6
Felix ASTO <felix_ab2@hotmail.com>
2024-06-01 12:03:41 -0500

    feat: spec fail

---
+++ src/app/operation/math.spec.ts
@@ -0,0 +1,13 @@
+import { sum } from './math';
+
+it('should return 5 when adding 2 and 3', () => {
+  expect(sum(2, 3)).toEqual(5);
+});
+
+it('should return -1 when adding -2 and 1', () => {
+  expect(sum(-2, 1)).toEqual(-1);
+});
+
+it('should return 0 when adding 0 and 0', () => {
+  expect(sum(1, 0)).toEqual(0);
+});
